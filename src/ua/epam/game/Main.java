b4bb5d69a39566bcd339137bf836a5a2f6bbe72b
Roman <student.rv86@gmail.com>
2019-11-30 20:25:00 +0200

    Fixed package name and String variables

--- src/ua/epam/game/Main.java
+++ src/ua/epam/game/Main.java
@@ -1,9 +1,14 @@
-package com.company;
+package ua.epam.game;
 
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+
+    private static final String START_MESSAGE = "Enter your number:";
+    private static final String HIGH_MESSAGE = "Too high!";
+    private static final String LOW_MESSAGE = "Too low!";
+    private static final String YOU_WIN_MESSAGE = "You win in %d attempts!";
 
     public static void main(String[] args) {
 
@@ -16,19 +21,19 @@ public class Main {
         int count = 0;
 
         while (!youWin) {
-            System.out.println("Enter your number:");
+            System.out.println(START_MESSAGE);
             int input = scanner.nextInt();
             count ++;
 
             if (input == guess) {
-                System.out.println("You win in " + count + " attempts!");
+                System.out.println(String.format(YOU_WIN_MESSAGE, count));
                 youWin = true;
             }
             if (input > guess) {
-                System.out.println("Too high!");
+                System.out.println(HIGH_MESSAGE);
             }
             if (input < guess) {
-                System.out.println("Too low!");
+                System.out.println(LOW_MESSAGE);
             }
         }
     }
